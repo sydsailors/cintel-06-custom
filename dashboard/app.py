91d727d3d571c2f82fe494b150ac0e9f70d743ce
Sydney Sailors <syd.sailors@gmail.com>
2025-07-30 17:56:37 -0500

    Update app.py file

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,7 +5,7 @@ import faicons as fa
 import pandas as pd
 from pathlib import Path
 from shiny import App, reactive, render
-from shiny.express import input, ui
+from shiny.express import input, ui, output
 from shinywidgets import output_widget, render_plotly
 import plotly.express as px
 from faicons import icon_svg
@@ -18,7 +18,7 @@ def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
         if df[col].dtype == object:
             df[col] = (
                 df[col]
-                .replace(r"[\$,]", "", regex=True)  # remove $ and commas
+                .replace(r"[\$%,]", "", regex=True)  # remove $ and commas
                 .replace("", None)  # convert empty strings to None
             )
             df[col] = pd.to_numeric(df[col], errors="ignore")  # convert if possible
@@ -38,7 +38,7 @@ rev_rng = (
 )
 
 # -----------------------------------
-# UI setup
+# UI sidebar
 # -----------------------------------
 ui.page_opts(title="Hospital Utilization - Four Quarter Summary", fillable=True)
 
@@ -51,3 +51,48 @@ with ui.sidebar():
         value=rev_rng,
         pre="$",
     )
+    ui.input_checkbox_group(
+        "year",
+        "Year",
+        sorted(hospital_df["Year"].unique().tolist()),
+        selected=sorted(hospital_df["Year"].unique().tolist()),
+        inline=True,
+    )
+    ui.input_action_button("reset", "Reset filter"),
+    open="desktop"
+
+# ----------------------------------
+# Icons
+# ----------------------------------
+ICONS = {
+    "hospital": fa.icon_svg("hospital"),
+    "money": fa.icon_svg("wallet"),
+    "percent": fa.icon_svg("percent"),
+    "ellipsis": fa.icon_svg("ellipsis"),
+}
+
+# ----------------------------------
+# UI setup
+# ----------------------------------
+with ui.layout_columns(fill=False):
+    with ui.value_box(showcase=ICONS["hospital"], theme="bg-gradient-purple-pink"):
+        "Number of Hospitals"
+
+        @render.express
+        def total_hospitals():
+            str(hospital_df.shape[0])
+        
+    with ui.value_box(showcase=ICONS["money"], theme="bg-gradient-pink-purple"):
+        "Average Revenue"
+
+        @render.express
+        def avg_revenue():
+            f"${hospital_df['Total Operating Rev'].mean():,.2f}"
+        
+    with ui.value_box(showcase=ICONS["percent"], theme="bg-gradient-purple-pink"):
+        "Average Operating Margin"
+
+        @render.express
+        def avg_margin():
+            f"{hospital_df['Operating Margin'].mean():.2%}"
+        
